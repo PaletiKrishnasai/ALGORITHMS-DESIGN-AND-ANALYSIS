cca170c31e873547a8a6ccb3e4146b5c2c233041
VamsiKrishnaThigulla <43385381+HikaruRaijin@users.noreply.github.com>
2020-03-28 16:49:57 +0530

    Update toh.cpp

--- toh.cpp
+++ toh.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void TOH(int n,int a ,int b, int c)
+/*void TOH(int n,int a ,int b, int c)
 {
     if(n>0)
     {
@@ -8,8 +8,21 @@ void TOH(int n,int a ,int b, int c)
         cout<<"move a disc from "<<a<<" to "<<c<<endl;
         TOH(n-1,b,a,c);
     }
-}
+}*/
 
+void TOH(int n,int a ,int b, int c)
+{
+	static int count=0;
+    if(n>0)
+    {
+        TOH(n-1,a,c,b);
+        //cout<<"move a disc from "<<a<<" to "<<c<<endl;
+        count++;
+        TOH(n-1,b,a,c);
+    }
+    if(n==0)
+		cout<<count<<endl;
+}
 
 int main()
 {
